9e38a4e8419bd5c48b839f4c42663719defb56ce
Jeffery To <jeffery.to@gmail.com>
2017-10-10 03:26:57 +0800

    more unnecessary imports

--- ex-mortis/windowstate.py
+++ ex-mortis/windowstate.py
@@ -20,7 +20,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from gi.repository import GObject, Gdk, Gio, Gedit
-from .utils import connect_handlers, disconnect_handlers
 from . import log
 
 
